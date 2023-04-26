e583df206a45ae9fd32e7871814f2b01e549d686
Maria0419 <maria.pazzini1@gmail.com>
2023-04-26 01:14:25 -0300

    Pagina de cadastro de vinhos

--- Vinhoteca/website/views.py
+++ Vinhoteca/website/views.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, request, flash, jsonify
+from flask import Blueprint, render_template, request, flash, jsonify, redirect, url_for
 from flask_login import login_required, current_user
 from .models import Note
 from .models import Vinho
@@ -12,19 +12,28 @@ views = Blueprint('views', __name__)
 @login_required
 def home():
     if request.method == 'POST': 
-        nome = request.form.get('nome')#Gets the note from the HTML 
-        safra = request.form.get('safra')
+        if 'register' in request.form: 
+            return redirect(url_for('views.register'))
+        
+        if 'add' in request.form:
+            nome = request.form.get('nome')#Gets the note from the HTML 
+            safra = request.form.get('safra')
 
-        if len(nome) < 1:
-            flash('Nome inválido!', category='error') 
-        else:
-            novo_vinho = Vinho(nome=nome, safra=safra, user_id=current_user.id)  #providing the schema for the note 
-            db.session.add(novo_vinho) #adding the note to the database 
-            db.session.commit()
-            flash('Vinho Adiconado!', category='success')
+            if len(nome) < 1:
+                flash('Nome inválido!', category='error') 
+            else:
+                novo_vinho = Vinho(nome=nome, safra=safra, user_id=current_user.id)  #providing the schema for the note 
+                db.session.add(novo_vinho) #adding the note to the database 
+                db.session.commit()
+                flash('Vinho Adiconado!', category='success')
 
     return render_template("home.html", user=current_user)
 
+
+@views.route('/cadastro', methods=['GET', 'POST'])
+@login_required
+def register():
+      return render_template("wine_register.html", user=current_user)
 
 @views.route('/delete-vinho', methods=['POST'])
 def delete_vinho():  
