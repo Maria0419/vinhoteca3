2a595b559aa6cf87f4b59aa8b9991600e7bc69f3
Maria0419 <maria.pazzini1@gmail.com>
2023-04-26 23:41:31 -0300

    banco de dados e cadastro

--- Vinhoteca/website/models.py
+++ Vinhoteca/website/models.py
@@ -3,18 +3,31 @@ from flask_login import UserMixin
 from sqlalchemy.sql import func
 
 
-class Note(db.Model):
+class Note(db.Model): #model
     id = db.Column(db.Integer, primary_key=True)
     data = db.Column(db.String(10000))
     date = db.Column(db.DateTime(timezone=True), default=func.now())
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
-class Vinho(db.Model):
+class Vinhos(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     nome = db.Column(db.String(100))
     safra = db.Column(db.Integer)
-    #data = db.Column(db.DateTime(timezone=True), default=func.now())
     tempodeguarda = db.Column(db.Integer)
+    harmonizacao = db.Column(db.String(100))
+    #adega_id = db.Column(db.String(100), db.ForeignKey('adega.id'))
+    #user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+vinho_inventario = db.Table('vinho_inventario',
+    db.Column('vinho_id', db.Integer, db.ForeignKey('vinhos.id'), primary_key=True),
+    db.Column('inventario_id', db.Integer, db.ForeignKey('inventario.id'), primary_key=True)
+)
+
+class Inventario(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    localizacao = db.Column(db.String(100))
+    estoque = db.Column(db.Integer)
+    #vinho = db.relationship('Vinhos')
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
 class User(db.Model, UserMixin):
@@ -22,5 +35,4 @@ class User(db.Model, UserMixin):
     email = db.Column(db.String(150), unique=True)
     password = db.Column(db.String(150))
     first_name = db.Column(db.String(150))
-    notes = db.relationship('Note')
-    vinhos = db.relationship('Vinho')
+    inventario = db.relationship('Inventario')
--- Vinhoteca/website/views.py
+++ Vinhoteca/website/views.py
@@ -1,7 +1,8 @@
 from flask import Blueprint, render_template, request, flash, jsonify, redirect, url_for
 from flask_login import login_required, current_user
 from .models import Note
-from .models import Vinho
+from .models import Vinhos
+from .models import Inventario
 from . import db
 import json
 
@@ -11,22 +12,6 @@ views = Blueprint('views', __name__)
 @views.route('/', methods=['GET', 'POST'])
 @login_required
 def home():
-    if request.method == 'POST': 
-        if 'register' in request.form: 
-            return redirect(url_for('views.register'))
-        
-        if 'add' in request.form:
-            nome = request.form.get('nome')#Gets the note from the HTML 
-            safra = request.form.get('safra')
-            tempodeguarda = request.form.get('tempodeguarda')
-
-            if len(nome) < 1:
-                flash('Nome inválido!', category='error') 
-            else:
-                novo_vinho = Vinho(nome=nome, safra=safra, user_id=current_user.id)  #providing the schema for the note 
-                db.session.add(novo_vinho) #adding the note to the database 
-                db.session.commit()
-                flash('Vinho Adiconado!', category='success')
 
     return render_template("home.html", user=current_user)
 
@@ -34,7 +19,26 @@ def home():
 @views.route('/cadastro', methods=['GET', 'POST'])
 @login_required
 def register():
-      return render_template("wine_register.html", user=current_user)
+    if request.method == 'POST': 
+        
+        if 'add' in request.form: 
+            return redirect(url_for('views.home'))
+        
+        if 'register' in request.form:
+            nome = request.form.get('nome')#Gets the note from the HTML 
+            safra = request.form.get('safra')
+            tempodeguarda = request.form.get('tempodeguarda')
+            harmonizacao = request.form.get('harmonizacao')
+            
+            if len(nome) < 1:
+                flash('Nome inválido!', category='error') 
+            else:
+                novo_vinho = Vinhos(nome=nome, safra=safra, tempodeguarda = tempodeguarda, harmonizacao=harmonizacao)  #providing the schema for the note 
+                db.session.add(novo_vinho) #adding the note to the database 
+                db.session.commit()
+                flash('Vinho Adiconado!', category='success')
+    vinhos = Vinhos.query.all()
+    return render_template("wine_register.html", vinhos=vinhos, user=current_user)
 
 @views.route('/harmonizacao', methods=['GET', 'POST'])
 @login_required
@@ -45,7 +49,7 @@ def pairing():
 def delete_vinho():  
     vinho = json.loads(request.data) # this function expects a JSON from the INDEX.js file 
     vinhoId = vinho['vinhoId']
-    vinho = Vinho.query.get(vinhoId)
+    vinho = Vinhos.query.get(vinhoId)
     if vinho:
         if vinho.user_id == current_user.id:
             db.session.delete(vinho)
